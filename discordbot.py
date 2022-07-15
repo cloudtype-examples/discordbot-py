4c9451c3a2eb2a36393285085874a691a00f6061
KYEONGMIN CHO <ggingmin@gmail.com>
2022-07-15 18:57:16 +0900

    first commit

---
+++ discordbot.py
@@ -0,0 +1,27 @@
+from distutils.sysconfig import PREFIX
+import discord
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+PREFIX = os.environ['PREFIX']
+TOKEN = os.environ['TOKEN']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} 가 로그인 하였습니다')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == f'{PREFIX}call':
+        await message.channel.send("callback!")
+
+    if message.content.startswith(f'{PREFIX}hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
